0caf0c32746e350de0c62ae5a7218d567153c895
Rafa Canosa <rafacv23@gmail.com>
2024-09-26 01:20:41 +0200

    added sample movies to mock the ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,73 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import Image from "next/image"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
+import Link from "next/link"
+
+const movies = [
+  {
+    slug: "spiderman-no-way-home",
+    title: "Spider-Man: No Way Home",
+    image: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+    description:
+      "Peter Parker busca la ayuda de Doctor Strange cuando su identidad es revelada.",
+  },
+  {
+    slug: "avengers-endgame",
+    title: "Avengers: Endgame",
+    image: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+    description:
+      "Los Vengadores se reúnen para intentar revertir las acciones de Thanos.",
+  },
+  {
+    slug: "the-batman",
+    title: "The Batman",
+    image: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    description:
+      "Bruce Wayne en su segundo año como Batman enfrenta a un asesino llamado el Acertijo.",
+  },
+  {
+    slug: "dune",
+    title: "Dune",
+    image: "https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
+    description:
+      "Un joven noble se convierte en el líder de un planeta desértico que es clave para la supervivencia del universo.",
+  },
+  {
+    slug: "no-time-to-die",
+    title: "No Time to Die",
+    image: "https://image.tmdb.org/t/p/w500/iUgygt3fscRoKWCV1d0C7FbM9TP.jpg",
+    description:
+      "James Bond ha dejado el servicio activo, pero su paz se ve interrumpida por un peligroso villano.",
+  },
+]
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Carousel
+          className="w-full max-w-xs"
           opts={{
-            align: "start",
+            align: "center",
+            loop: true,
           }}
-          className="w-full max-w-sm"
         >
           <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                <div className="p-1">Hello</div>
+            {movies.map((movie) => (
+              <CarouselItem key={movie.title}>
+                <Link href={`/${movie.slug}`}>
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <img src={movie.image} />
+                    </CardContent>
+                    <CardFooter>
+                      <span className="text-4xl font-semibold">
+                        {movie.title}
+                      </span>
+                    </CardFooter>
+                  </Card>
+                </Link>
               </CarouselItem>
             ))}
           </CarouselContent>
