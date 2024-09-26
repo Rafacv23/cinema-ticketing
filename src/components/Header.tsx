5e8dbd0d5c5ecbdb676ca1f1bfbe373189580b8b
Rafa Canosa <rafacv23@gmail.com>
2024-09-26 16:36:55 +0200

    sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   const pathname = usePathname()
 
   return (
-    <header className="flex justify-center space-x-4 items-center p-4">
+    <header className="backdrop-blur-lg backdrop-opacity-60 bg-white/60 dark:bg-slate-900/60 flex justify-center space-x-4 items-center p-4 sticky top-0 z-40 w-full border-b border-b-slate-200 dark:border-b-slate-700">
       <div className="flex-grow flex justify-center space-x-4 items-center">
         <Link
           href={"/"}
