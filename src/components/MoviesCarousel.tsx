8384cf00b6d1e18755483e0a4db93524ad281fcc
Rafa Canosa <rafacv23@gmail.com>
2024-09-26 16:37:05 +0200

    removed carousel

--- src/components/MoviesCarousel.tsx
+++ src/components/MoviesCarousel.tsx
@@ -1,45 +1,20 @@
-import Link from "next/link"
-import { Card, CardContent, CardFooter } from "@/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
 import { Movie } from "@/lib/types"
+import MovieCard from "./MovieCard"
 
-interface MoviesCarouselProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface MoviesCarouselProps
+  extends React.HTMLAttributes<HTMLDivElement> {
   movies: Movie[]
 }
 
 export default function MoviesCarousel({ movies }: MoviesCarouselProps) {
   return (
-    <Carousel
-      className="w-full max-w-xs md:max-w-xl"
-      opts={{
-        align: "center",
-        loop: true,
-      }}
-    >
-      <CarouselContent>
+    <div className="w-full">
+      <h2 className="text-3xl font-bold mb-6">Now in theaters</h2>
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {movies.map((movie) => (
-          <CarouselItem key={movie.title} className="md:basis-2/3">
-            <Link href={`/${movie.slug}`}>
-              <Card className="hover:text-accent transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  shadow-xl">
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <img src={movie.img} className="rounded-xl" />
-                </CardContent>
-                <CardFooter>
-                  <h2 className="text-2xl font-semibold">{movie.title}</h2>
-                </CardFooter>
-              </Card>
-            </Link>
-          </CarouselItem>
+          <MovieCard key={movie.slug} movie={movie} />
         ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      </ul>
+    </div>
   )
 }
