013bcd249ea5bb8c4717c7376945513fa588877d
Rafa Canosa <rafacv23@gmail.com>
2024-09-27 02:23:35 +0200

    protected page to admin users

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,15 @@
-export default function ProtectPage() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { notFound } from "next/navigation"
+
+export default async function ProtectPage() {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL
+
+  if (!user || user.email !== ADMIN_EMAIL) {
+    return notFound()
+  }
+
   return <div>admin panel</div>
 }
