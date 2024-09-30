fa49e8efe6a4b401a74f5d74246425ce349ee806
Rafa Canosa <rafacv23@gmail.com>
2024-09-30 16:53:57 +0200

    add dynamical rendering

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -4,12 +4,18 @@ import MovieCard from "./MovieCard"
 export interface MoviesCarouselProps
   extends React.HTMLAttributes<HTMLDivElement> {
   movies: Movie[]
+  inTheaters: boolean
 }
 
-export default function MoviesList({ movies }: MoviesCarouselProps) {
+export default function MoviesList({
+  movies,
+  inTheaters,
+}: MoviesCarouselProps) {
   return (
     <div className="w-full">
-      <h2 className="text-3xl font-bold mb-6">Now in theaters</h2>
+      <h2 className="text-3xl font-bold mb-6">
+        {inTheaters ? "Now in theaters" : "Coming soon"}
+      </h2>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {movies.map((movie) => (
           <MovieCard key={movie.slug} movie={movie} />
