2ca71bfc96bd3dd13998543da97dd3813996b4fe
Rafa Canosa <rafacv23@gmail.com>
2024-09-30 17:14:00 +0200

    logic to verify if a user exists or not moved to a new api route

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,30 +4,18 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { PrismaClient } from "@prisma/client"
 
 export default async function Login() {
-  const prisma = new PrismaClient()
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
   if (user) {
-    const existingUser = await prisma.user.findUnique({
-      where: { id: user.id },
+    await fetch(`http://localhost:3000/api/auth/isRegister`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    if (!existingUser) {
-      // Si no existe, creamos un nuevo usuario
-      const newUser = await prisma.user.create({
-        data: {
-          id: user.id,
-          email: user.email,
-          name: `${user.given_name} ${user.family_name}`,
-        },
-      })
-      console.log("Nuevo usuario creado:", newUser)
-    } else {
-      console.log("Usuario ya existe:", existingUser)
-    }
   }
 
   return user ? (
