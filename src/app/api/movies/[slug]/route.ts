c115ecfcde10f4e43e9e823fba428dc18c8672e7
Rafa Canosa <rafacv23@gmail.com>
2024-09-30 16:53:03 +0200

    created

---
+++ src/app/api/movies/[slug]/route.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    // Obtener el slug de los parámetros
+    const slug = params.slug
+
+    // Buscar la película por título
+    const movie = await prisma.movie.findUnique({
+      where: { slug: slug }, // Querying by title
+    })
+
+    if (!movie) {
+      return new Response("Movie not found", { status: 404 })
+    }
+
+    // Retornar la película en formato JSON
+    return new Response(JSON.stringify(movie), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } catch (error) {
+    console.error(error)
+    return new Response("Error retrieving movie", { status: 500 })
+  } finally {
+    await prisma.$disconnect() // Asegúrate de desconectar el cliente Prisma
+  }
+}
---
+++ src/app/api/movies/soon/route.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client"
+
+export async function GET() {
+  const prisma = new PrismaClient()
+
+  try {
+    const currentDate = new Date()
+    const movies = await prisma.movie.findMany({
+      where: { release: { gt: currentDate } },
+    })
+
+    return new Response(JSON.stringify(movies), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } catch (error) {
+    console.error(error)
+    return new Response(JSON.stringify({ error: "An error occurred" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
