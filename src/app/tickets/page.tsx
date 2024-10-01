cf54f08593d29500cf336d19b44a5b7f2ebbdace
Rafa Canosa <rafacv23@gmail.com>
2024-10-01 18:34:30 +0200

    error handling if user is not logged

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -22,8 +22,18 @@ export default async function Page() {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
+  if (!user) {
+    return <div>User not found. Please log in.</div>
+  }
+
   const res = await fetch(`http://localhost:3000/api/tickets/${user.id}`)
-  const ticketsPerUser = await res.json()
+
+  // Check if response is ok
+  if (!res.ok) {
+    return <div>Error fetching tickets.</div>
+  }
+
+  const ticketsPerUser: Ticket[] = await res.json()
 
   return (
     <Table>
@@ -38,19 +48,27 @@ export default async function Page() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {ticketsPerUser.map((ticket: Ticket) => (
-          <TableRow key={ticket.id}>
-            <TableCell className="font-medium">{ticket.id}</TableCell>
-            <TableCell>{ticket.status}</TableCell>
-            <TableCell>{ticket.movieId}</TableCell>
-            <TableCell className="text-right">
-              {ticket.seats.join(", ")}
-            </TableCell>
-            <TableCell className="text-right">
-              {formatDate(ticket.date)}
+        {ticketsPerUser.length === 0 ? (
+          <TableRow>
+            <TableCell colSpan={5} className="text-center">
+              No tickets found.
             </TableCell>
           </TableRow>
-        ))}
+        ) : (
+          ticketsPerUser.map((ticket: Ticket) => (
+            <TableRow key={ticket.id}>
+              <TableCell className="font-medium">{ticket.id}</TableCell>
+              <TableCell>{ticket.status}</TableCell>
+              <TableCell>{ticket.movieId}</TableCell>
+              <TableCell className="text-right">
+                {ticket.seats.join(", ")}
+              </TableCell>
+              <TableCell className="text-right">
+                {formatDate(ticket.date)}
+              </TableCell>
+            </TableRow>
+          ))
+        )}
       </TableBody>
     </Table>
   )
