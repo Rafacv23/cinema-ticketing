3348e6db4a55df737848ee92a6415e444a4a858d
Rafa Canosa <rafacv23@gmail.com>
2024-10-01 17:42:01 +0200

    pages to show the recent tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -9,25 +9,25 @@ import {
 } from "@/components/ui/table"
 import { formatDate } from "@/lib/utils"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { Ticket } from "@prisma/client"
-import { notFound } from "next/navigation"
 
-export default async function ProtectPage() {
+interface Ticket {
+  id: string
+  status: string
+  movieId: string
+  seats: string[]
+  date: Date
+}
+
+export default async function Page() {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
 
-  const ADMIN_EMAIL = process.env.ADMIN_EMAIL
-
-  if (!user || user.email !== ADMIN_EMAIL) {
-    return notFound()
-  }
-
-  const res = await fetch(`http://localhost:3000/api/tickets/admin`)
-  const tickets = await res.json()
+  const res = await fetch(`http://localhost:3000/api/tickets/${user.id}`)
+  const ticketsPerUser = await res.json()
 
   return (
     <Table>
-      <TableCaption>A list of all tickets.</TableCaption>
+      <TableCaption>A list of your recent tickets.</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">Order Number</TableHead>
@@ -38,7 +38,7 @@ export default async function ProtectPage() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {tickets.map((ticket: Ticket) => (
+        {ticketsPerUser.map((ticket: Ticket) => (
           <TableRow key={ticket.id}>
             <TableCell className="font-medium">{ticket.id}</TableCell>
             <TableCell>{ticket.status}</TableCell>
