1d0cd6aeccb43c00c0e2afa6fc4455aae1687d45
Rafa Canosa <rafacv23@gmail.com>
2024-10-03 18:03:30 +0200

    dynamical rendering

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -6,12 +6,21 @@ import { buttonVariants } from "@/components/ui/button"
 import { formatDate } from "@/lib/utils"
 import StickyBuyBtn from "@/components/StickyBuyBtn"
 import YouTubePlayer from "@/components/YoutubePlayer"
+import CartBtn from "@/components/CartBtn"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import BackBtn from "@/components/BackBtn"
 
 export default async function Page({ params }: { params: { slug: string } }) {
   // Fetch the movie data based on the slug
   const response = await fetch(
     `http://localhost:3000/api/movies/${params.slug}`
   )
+
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+  let isLogged = false
+
+  if (user) isLogged = true
 
   // Check if the response is ok
   if (!response.ok) {
@@ -23,15 +32,20 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start">
+        <BackBtn url={`/`} />
         <YouTubePlayer movieTrailer={movie.trailer} />
         <div className="flex justify-between w-full items-center mb-4">
           <h1 className="text-3xl font-bold">{movie.title}</h1>
-          <Link
-            href={`/movie/${movie.slug}/cart`}
-            className={buttonVariants({ variant: "default" })}
-          >
-            Buy tickets
-          </Link>
+          {user ? (
+            <Link
+              href={`/movie/${movie.slug}/cart`}
+              className={buttonVariants({ variant: "default" })}
+            >
+              Buy tickets
+            </Link>
+          ) : (
+            <CartBtn />
+          )}
           <span className="flex items-center gap-2">
             <Timer /> {movie.duration} min
           </span>
@@ -58,7 +72,11 @@ export default async function Page({ params }: { params: { slug: string } }) {
         <p>{movie.director}</p>
         <h2 className="text-xl font-bold">Cast</h2>
         <p>{movie.cast.join(", ")}</p>
-        <StickyBuyBtn movieSlug={movie.slug} movieTitle={movie.title} />
+        <StickyBuyBtn
+          movieSlug={movie.slug}
+          movieTitle={movie.title}
+          isLogged={isLogged}
+        />
       </div>
     </div>
   )
