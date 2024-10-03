c8ac14bff92662f2885890722d0d35209f2b82c3
Rafa Canosa <rafacv23@gmail.com>
2024-10-03 18:02:25 +0200

    create movie function for admins only

---
+++ src/app/admin/create/actions.ts
@@ -0,0 +1,44 @@
+"use server"
+
+import { formSchema } from "@/components/AddMovieForm"
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client"
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
+
+const prisma = new PrismaClient()
+
+export const createMovie = async (values: z.infer<typeof formSchema>) => {
+  try {
+    const releaseDate = new Date(values.release).toISOString()
+    const endDate = new Date(values.endDate).toISOString()
+
+    const movie = await prisma.movie.create({
+      data: {
+        title: values.title,
+        slug: values.title.toLowerCase().replaceAll(" ", "-"),
+        description: values.description,
+        poster: values.poster,
+        trailer: values.trailer,
+        genres: values.genres,
+        duration: values.duration,
+        release: releaseDate,
+        director: values.director,
+        cast: values.cast,
+        price: values.price,
+        endDate: endDate,
+      },
+    })
+
+    console.log(`Movie created successfully:`, movie)
+    revalidatePath("/")
+    revalidatePath("/coming-soon")
+    redirect("/admin")
+    return { success: true, message: "Movie created successfully" }
+  } catch (error) {
+    console.error("Error creating movie:", error)
+    return { success: false, message: "Failed to create movie" }
+  } finally {
+    await prisma.$disconnect()
+  }
+}
