49a15f2aa171fe8d18fbad16e0f6c8e36bfbeb55
Rafa Canosa <rafacv23@gmail.com>
2024-10-03 18:02:13 +0200

    new order of the table columns

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+import BackBtn from "@/components/BackBtn"
+import { Button, buttonVariants } from "@/components/ui/button"
 import {
   Table,
   TableBody,
@@ -10,6 +12,8 @@ import {
 import { formatDate } from "@/lib/utils"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { Ticket } from "@prisma/client"
+import { CircleX } from "lucide-react"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 export default async function ProtectPage() {
@@ -26,32 +30,44 @@ export default async function ProtectPage() {
   const tickets = await res.json()
 
   return (
-    <Table>
-      <TableCaption>A list of all tickets.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Order Number</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Movie</TableHead>
-          <TableHead className="text-right">Seats</TableHead>
-          <TableHead className="text-right">Date</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {tickets.map((ticket: Ticket) => (
-          <TableRow key={ticket.id}>
-            <TableCell className="font-medium">{ticket.id}</TableCell>
-            <TableCell>{ticket.status}</TableCell>
-            <TableCell>{ticket.movieId}</TableCell>
-            <TableCell className="text-right">
-              {ticket.seats.join(", ")}
-            </TableCell>
-            <TableCell className="text-right">
-              {formatDate(ticket.date)}
-            </TableCell>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header>
+        <BackBtn url="/" />
+        <Link
+          href="/admin/create"
+          className={buttonVariants({ variant: "default" })}
+        >
+          Add a new movie
+        </Link>
+      </header>
+      <Table>
+        <TableCaption>A list of all tickets.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Order</TableHead>
+            <TableHead>Movie</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Seats</TableHead>
+            <TableHead>Date</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {tickets.map((ticket: Ticket) => (
+            <TableRow key={ticket.id}>
+              <TableCell className="font-medium">{ticket.id}</TableCell>
+              <TableCell>{ticket.movieId}</TableCell>
+              <TableCell>{ticket.status}</TableCell>
+              <TableCell>{ticket.seats.join(", ")}</TableCell>
+              <TableCell>{formatDate(ticket.date)}</TableCell>
+              <TableCell className="text-right">
+                <Button variant={"destructive"}>
+                  <CircleX />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
