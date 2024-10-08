f5b9c098bc54a893a814c3cab6f20adc335b9a10
Rafa Canosa <rafacv23@gmail.com>
2024-10-08 16:52:30 +0200

    update movie page

---
+++ src/app/admin/manage/update/page.tsx
@@ -0,0 +1,10 @@
+import BackBtn from "@/components/BackBtn"
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Update Movie</h1>
+      <BackBtn url="/admin/manage" />
+    </div>
+  )
+}
