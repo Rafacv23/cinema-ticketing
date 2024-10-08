8aaa3bd755f2a14a6190918f338ada470333f928
Rafa Canosa <rafacv23@gmail.com>
2024-10-08 16:52:24 +0200

    delete movie page

---
+++ src/app/admin/manage/delete/page.tsx
@@ -0,0 +1,27 @@
+import BackBtn from "@/components/BackBtn"
+import { Movie } from "../page"
+import DeleteMovieAlert from "@/components/DeleteMovieAlert"
+
+export default async function Page() {
+  const res = await fetch(`http://localhost:3000/api/movies`)
+
+  if (!res.ok) {
+    return <div>Error fetching movies.</div>
+  }
+
+  const movies = await res.json()
+
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">Delete Movie</h1>
+      <BackBtn url="/admin/manage" />
+      <ul className="my-4 flex flex-col items-start gap-4">
+        {movies.map((movie: Movie) => (
+          <li key={movie.id}>
+            <DeleteMovieAlert slug={movie.slug} title={movie.title} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
