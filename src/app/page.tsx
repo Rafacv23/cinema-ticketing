a44b54de8d36bc5b4e3f2f76540d56399a0403a9
Rafa Canosa <rafacv23@gmail.com>
2024-10-10 16:45:45 +0200

    new urls for fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import MoviesList from "@/components/MoviesList"
+import { SITE_URL } from "@/site/config"
 
 export default async function Home() {
-  const movies = await fetch("http://localhost:3000/api/movies/current").then(
-    (res) => res.json()
+  const movies = await fetch(`${SITE_URL}/api/movies/current`).then((res) =>
+    res.json()
   )
 
   return <MoviesList inTheaters={true} movies={movies} />
