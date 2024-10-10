c27db9f693c9bc1fa2e24382247c9f8e9eafa1e9
Rafa Canosa <rafacv23@gmail.com>
2024-10-10 17:12:26 +0200

    new fetching

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -2,9 +2,9 @@ import MoviesList from "@/components/MoviesList"
 import { SITE_URL } from "@/site/config"
 
 export default async function Home() {
-  const movies = await fetch(`${SITE_URL}/api/movies/soon`).then((res) =>
-    res.json()
-  )
+  const res = await fetch(`${SITE_URL}/api/movies/soon`)
+
+  const movies = await res.json()
 
   return <MoviesList inTheaters={false} movies={movies} />
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import MoviesList from "@/components/MoviesList"
 import { SITE_URL } from "@/site/config"
 
 export default async function Home() {
-  const movies = await fetch(`${SITE_URL}/api/movies/current`).then((res) =>
-    res.json()
-  )
+  const res = await fetch(`${SITE_URL}/api/movies/current`)
+
+  const movies = await res.json()
 
   return <MoviesList inTheaters={true} movies={movies} />
 }
