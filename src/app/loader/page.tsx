a90bf2c533ce6615c78e81345ab3a8d220b00167
Rafa Canosa <rafacv23@gmail.com>
2024-10-14 17:10:56 +0200

    loader

---
+++ src/app/loader/page.tsx
@@ -0,0 +1,9 @@
+import Loader from "@/components/Loader"
+
+export default function PAge() {
+  return (
+    <div className="col-span-5 md:col-span-3 row-span-3 md:col-start-2 row-start-2 mb-16 mx-auto">
+      <Loader />
+    </div>
+  )
+}
