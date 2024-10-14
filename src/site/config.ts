1a4fc8941675c343ad5f6f2af994b9b50fe0d087
Rafa Canosa <rafacv23@gmail.com>
2024-10-14 17:21:26 +0200

    updated title

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,6 +1,5 @@
 export const SITE_NAME = "CinePass"
-export const SITE_TITLE =
-  "CinePass | Open Source Movie Ticketing App made with Next.js"
+export const SITE_TITLE = "CinePass | Book Movie Tickets Online"
 export const SITE_DESCRIPTION =
   "Discover the latest movies and book your tickets effortlessly with CinePass. Enjoy a seamless cinema experience with real-time seat selection and exclusive offers. Join us today to catch your favorite films on the big screen!"
 export const SITE_URL = "https://cinepass-jade.vercel.app"
