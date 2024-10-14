40b18dc79121bfebdaed77c428311ed3439c5a4b
Rafa Canosa <rafacv23@gmail.com>
2024-10-14 17:11:43 +0200

    navigation

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,12 @@
 import BackBtn from "@/components/buttons/BackBtn"
 import TicketsTable from "@/components/TicketsTable"
+import { buttonVariants } from "@/components/ui/button"
 import { SITE_URL } from "@/site/config"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import {
+  getKindeServerSession,
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/server"
 
 export interface Ticket {
   id: string
@@ -19,7 +24,20 @@ export default async function Page() {
   const user = await getUser()
 
   if (!user) {
-    return <div>User not found. Please log in.</div>
+    return (
+      <div className="flex flex-col gap-4">
+        <BackBtn url="/" />
+        User not found.
+        <div className="flex justify-between">
+          <LoginLink className={buttonVariants({ variant: "default" })}>
+            Log in
+          </LoginLink>
+          <RegisterLink className={buttonVariants({ variant: "outline" })}>
+            Register
+          </RegisterLink>
+        </div>
+      </div>
+    )
   }
 
   const res = await fetch(`${SITE_URL}/api/tickets/${user.id}`)
