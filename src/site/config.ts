db4e67e8fb9fa0cf922819bed33506f425eec591
Rafa Canosa <rafacv23@gmail.com>
2024-10-14 17:12:10 +0200

    title for seo

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,4 +1,6 @@
 export const SITE_NAME = "CinePass"
+export const SITE_TITLE =
+  "CinePass | Open Source Movie Ticketing App made with Next.js"
 export const SITE_URL = "https://cinepass-jade.vercel.app"
 export const GITHUB_URL = "https://github.com/rafacv23/cinema-ticketing"
 export const LINKEDIN_URL = "https://ninjapath.vercel.app/linkedin"
